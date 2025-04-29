d87af735395c7af5e3e87429fe294cb93810eb2f
ReemAbed100 <151785644+ReemAbed100@users.noreply.github.com>
2025-04-29 15:05:26 +0300

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -2,27 +2,27 @@
 const logos = {
   1976: {
     src: "Apple-1976-logo.png",
-    description: "The very first Apple logo in 1976 featured Isaac Newton under an apple tree. It was designed by Ronald Wayne."
+    description: "The very first Apple logo, created by co-founder Ronald Wayne, depicted Sir Isaac Newton sitting under an apple tree, referencing the story of gravity's discovery. This intricate design was short-lived and replaced the following year."
   },
   1977: {
     src: "Apple-1977-logo.png",
-    description: "Apple introduced a cleaner, more modern look with a monochrome logo."
+    description: "Apple introduced a simpler and more modern design in 1977, crafted by Rob Janoff. This logo featured a stylized apple with a bite taken out of it, symbolizing knowledge and simplicity, which aligned with Apple's mission to make computers more user-friendly."
   },
   1998: {
     src: "Apple-1998-logo.png",
-    description: "The very first Apple logo in 1976 featured Isaac Newton under an apple tree. It was designed by Ronald Wayne."
+    description: "As Apple transitioned to a new era under Steve Jobs' leadership, the logo received a smoother, more minimalistic look. This logo design was created with a polished, monochromatic finish to emphasize Apple's new direction as a forward-thinking tech company."
   },
   2001: {
     src: "Apple-2001-logo.png",
-    description: "The logo evolved to a flatter, minimal design, reflecting modern UI trends."
+    description: "In 2001, as Apple became a global brand, the logo was given a metallic sheen to reflect its premium products. The new design, introduced alongside the iPod and other breakthrough products, was intended to reflect a blend of technology and elegance."
   },
   2007: {
     src: "Apple-2007-logo.png",
-    description: "A refined version with a sleeker finish, maintaining the iconic apple shape."
+    description: "ith the launch of the iconic iPhone in 2007, Apple embraced a reflective glass effect in its logo. This sleek, shiny appearance echoed Apple's commitment to creating beautiful and functional products, with the logo symbolizing innovation and cutting-edge technology."
   },
   2017: {
     src: "Apple-2017-logo.png",
-    description: "The latest design continues Apple's commitment to simplicity and elegance."
+    description: "The most recent Apple logo, introduced in 2017, returned to a simple, flat design. This design reflects Apple's refined focus on clean aesthetics and its core principle of creating simple, intuitive products that remain timeless in their design."
   },
 };
 
